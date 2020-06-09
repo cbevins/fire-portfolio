788ec17ecd9abb389b443b3e96dcc21ef6a9a140
Collin D. Bevins <cbevins@montana.com>
2020-06-09 14:40:52 -0600

    ADded font-awesome icons

--- src/components/Tree/Tree.svelte
+++ src/components/Tree/Tree.svelte
@@ -2,13 +2,45 @@
   import { _selected } from './modulesStore.js'
   import { modules, variableMap } from './modules.js'
   import Branch from './Branch.svelte';
+  import { Button, Collapse,
+    Card, CardBody, CardFooter, CardHeader,
+    CardImg, CardSubtitle, CardText, CardTitle } from "sveltestrap";
+  let isOpen = false
 </script>
 
-Currently selected:
+
+<div class="overflow-auto" max-height="100">
+<Card class="mb-3">
+  <CardHeader>
+    <Button color="primary" size="sm"
+        on:click={() => (isOpen = !isOpen)} class="mb-3">
+        {#if isOpen}
+        <span class="fa fa-toggle-up"></span>
+        {:else}
+        <span class="fa fa-toggle-down"></span>
+        {/if}
+    </Button>
+    Currently {$_selected.length} Selected Output Variables
+  </CardHeader>
+  <Collapse {isOpen}>
+  <CardBody>
   <ul>
     {#each $_selected.sort() as key}
       <li>{variableMap.get(key).label}</li>
     {/each}
   </ul>
+  </CardBody>
+  </Collapse>
+</Card>
+</div>
 
-<Branch label="Home" items={modules} expanded/>
+<div class="overflow-auto" max-height="800">
+  <Card>
+  <CardHeader>
+    Select Variables Here
+  </CardHeader>
+  <CardBody>
+    <Branch label="Home" items={modules} expanded/>
+  </CardBody>
+  </Card>
+</div>
